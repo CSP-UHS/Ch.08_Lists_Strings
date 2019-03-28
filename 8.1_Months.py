a01132922908940fb3de981765879aa078f5acf2
OrigamiElephant <mcculleyezr@urbandaleschools.com>
2019-03-28 11:43:10 -0500

    Months Program

--- 8.1_Months.py
+++ 8.1_Months.py
@@ -9,3 +9,13 @@ Once the user quits, print "Goodbye!"
 months = "JanFebMarAprMayJunJulAugSepOctNovDec"
 
 '''
+y = 1
+while y == 1:
+    months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec", "Goodbye"]
+    print("Put in a number between 1 - 12 to receive the corresponding month, or enter 13 to quit.")
+    x = int(input("Enter:"))
+    if x < 13:
+        print(months[x-1])
+    else:
+        print(months[12])
+        y = 0
