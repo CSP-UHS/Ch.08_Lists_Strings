0666a1e79fc01f8834e3b0bfcdde8a127f116c17
OrigamiElephant <mcculleyezr@urbandaleschools.com>
2019-04-05 12:19:04 -0500

    Figuring out lists

--- 8.3_Adventure.py
+++ 8.3_Adventure.py
@@ -6,31 +6,54 @@ ADVENTURE PROGRAM
 3.) Expand your program to make it a real adventure game
 
 '''
+room0 = ()
+room1 = ()
+room2 = ()
+room3 = ()
+room4 = ()
+room5 = ()
+room6 = ()
+room7 = ()
+room8 = ()
+room9 = ()
+CR = room0
 done = "false"
 dire = 0
 none = 0
 inside = 1
 room_list = []
-room0 = ["You are standing on an old rickety porch", 1, none, none, none]
-room_list.append(room0)
-room1 = ["You walk into a large entrance wih chairs and tables covered in dust strewn about.", 1, 2, 3, 4]
-room_list.append(room1)
-room2 = ["Empty chairs and a broken sofa lie in the middle of what as a living room", none, 2, none, none]
-room_list.append(room2)
-room3 = ["It's a kitchen with pots and pans all over the place. There's water all over the floor.", none, none, none, 4]
-room_list.append(room3)
-room4 = ["You see a long, dark hallway with two broken doors at either sides of you.", 1, 2, 3, 4]
-room_list.append(room4)
-room5 = ["You walk further into the hallway, reaching two doors at your sides and one at the very end.", 1, 2, 3, 4]
-room_list.append(room5)
-room6 = ["There are shelves lining the walls with small scraps of food on them, looks like a pantry", 1, 2, none, none]
-room_list.append(room6)
-room7 = ["It's a dirty bathroom with a shattered toilet and a faucet slowly dripping water", 1, none, none, 4]
-room_list.append(room7)
-room8 = ["You enter a bedroom with two beds on either side of the room", none, 2, 3, none]
-room_list.append(room8)
-room9 = ["There's a large bed in the corner of the room with some liquid leaking from the ceiling", none, none, 3, 4]
-room_list.append(room9)
+for l in range(2):
+    room0 = ["You are standing on an old rickety porch", room1, none, none, none]
+    room_list.append(room0)
+    room1 = ["You walk into a large entrance wih chairs and tables covered in dust strewn about.", 1, 2, 3, 4]
+    room_list.append(room1)
+    room2 = ["Empty chairs and a broken sofa lie in the middle of what as a living room", none, 2, none, none]
+    room_list.append(room2)
+    room3 = ["It's a kitchen with pots and pans all over the place. There's water all over the floor.", none, none, none, 4]
+    room_list.append(room3)
+    room4 = ["You see a long, dark hallway with two broken doors at either sides of you.", 1, 2, 3, 4]
+    room_list.append(room4)
+    room5 = ["You walk further into the hallway, reaching two doors at your sides and one at the very end.", 1, 2, 3, 4]
+    room_list.append(room5)
+    room6 = ["There are shelves lining the walls with small scraps of food on them, looks like a pantry", 1, 2, none, none]
+    room_list.append(room6)
+    room7 = ["It's a dirty bathroom with a shattered toilet and a faucet slowly dripping water", 1, none, none, 4]
+    room_list.append(room7)
+    room8 = ["You enter a bedroom with two beds on either side of the room", none, 2, 3, none]
+    room_list.append(room8)
+    room9 = ["There's a large bed in the corner of the room with some liquid leaking from the ceiling", none, none, 3, 4]
+    room_list.append(room9)
+CR = room0
+print(room0[0])
+dire = input("where would you like to go? (N)orth (E)ast (S)outh (W)est (Q)uit")
+if dire == "N":
+    CR = CR[1]
+    print(CR[0])
+elif dire == "E":
+    CR = CR[2]
+    print(CR[0])
+
+    '''
 print(room0[0])
 C_R = room0
 while done == "false":
@@ -60,4 +83,4 @@ while done == "false":
     elif C_R == room8:
 
     elif C_R == room9:
-
+'''
