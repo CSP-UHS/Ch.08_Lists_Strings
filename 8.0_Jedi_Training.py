a5453e6243002c21bbe4738b2c4dfaf1e62d3140
smithdyl <smithdyl@urbandaleschools.com>
2019-11-21 13:15:36 -0600

    Training

--- 8.0_Jedi_Training.py
+++ 8.0_Jedi_Training.py
@@ -1,4 +1,4 @@
-#Sign your name:________________
+#Sign your name: Dylan Smith
 
 '''
 1.)
@@ -8,19 +8,33 @@ Don't use that. Sum the numbers individually as shown in the chapter.
 Also, a common mistake is to calculate the average each time through the loop 
 to add the numbers. Finish adding the numbers before you divide.
 '''
+'''
 a_list = [3,12,3,5,3,4,6,8,5,3,5,6,3,2,4]
 b_list = [4,15,2,7,8,3,1,10,9]
 c_list = [5,10,13,12,5,9,2,6,1,8,8,9,11,13,14,8,2,2,6,3,9,8,10]
-
-
-
+a=input("What list do you want?");total=0
+if a.lower()=="a":
+    L=a_list
+elif a.lower()=="b":
+    L=b_list
+elif a.lower()=="c":
+    L=c_list
+for item in L:
+    total+=item
+print("The average is", total//len(L))
+'''
 '''
 2.) Write a program that will strip the username (whatever is in front of the @ symbol)
 from any e-mail address and print it. First ask the user for their e-mail address.
 '''
+'''
+a=input("What is your email")
+print("Gimme a sec...")
+result=a.find("@")
+d=str.find(a,"@",result)
+print("Your username is",a[:d])
 
-
-
+'''
 '''
 TEXT FORMATTING:
 3.) Make following program output the following:
@@ -30,10 +44,10 @@ TEXT FORMATTING:
      
      Do not use any plus sign (+) in your code.
      You should only have two double quotes in each print statement.
-     '''
+'''
 score = 41237
 highscore = 1023407
-print("Score:      " + str(score) )
-print("High score: " + str(highscore) )
+print("Score:      ", "{:,}".format(score) )
+print("High score: ", "{:,}".format(highscore) )
 
 
--- 8.1_Months.py
+++ 8.1_Months.py
@@ -9,3 +9,48 @@ Once the user quits, print "Goodbye!"
 months = "JanFebMarAprMayJunJulAugSepOctNovDec"
 
 '''
+months = "JanFebMarAprMayJunJulAugSepOctNovDec"
+times=0
+done=False
+while done is False:
+    print()
+    month = int(input("Pick a number 1-12 for a month or 13 to quit\n"))
+    if month==1:
+        print("The month you chose abv. is",months[0:3])
+        times+=1
+    elif month==2:
+        print("The month you chose abv. is",months[3:6])
+        times += 1
+    elif month==3:
+        print("The month you chose abv. is",months[6:9])
+        times += 1
+    elif month==4:
+        print("The month you chose abv. is",months[9:12])
+        times += 1
+    elif month==5:
+        print("The month you chose abv. is",months[12:15])
+        times+=1
+    elif month==6:
+        print("The month you chose abv. is",months[15:18])
+        times += 1
+    elif month==7:
+        print("The month you chose abv. is",months[18:21])
+        times += 1
+    elif month==8:
+        print("The month you chose abv. is",months[21:24])
+        times += 1
+    elif month==9:
+        print("The month you chose abv. is",months[24:27])
+        times += 1
+    elif month==10:
+        print("The month you chose abv. is",months[27:30])
+        times += 1
+    elif month==11:
+        print("The month you chose abv. is",months[30:33])
+        times+=1
+    elif month==12:
+        print("The month you chose abv. is",months[33:36])
+        times += 1
+    else:
+        print("You used the program",times,"times!\nThank you for your time.")
+        done=True
