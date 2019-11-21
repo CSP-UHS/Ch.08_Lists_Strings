1c87d2874e2b059d71db2e51f5f3d0c01d1c7c00
leavertonnel <leavertonnel@urbandaleschools.com>
2019-11-21 13:16:45 -0600

    just a commit

--- 8.0_Jedi_Training.py
+++ 8.0_Jedi_Training.py
@@ -7,29 +7,30 @@ Use the len function. There is a sum function I haven't told you about.
 Don't use that. Sum the numbers individually as shown in the chapter. 
 Also, a common mistake is to calculate the average each time through the loop 
 to add the numbers. Finish adding the numbers before you divide.
-'''
-a_list = [3,12,3,5,3,4,6,8,5,3,5,6,3,2,4]
-b_list = [4,15,2,7,8,3,1,10,9]
-c_list = [5,10,13,12,5,9,2,6,1,8,8,9,11,13,14,8,2,2,6,3,9,8,10]
-
-list_total = 0
-
-for item in a_list:
-    list_total += item/len(a_list)
-print(list_total)
-
-for item in b_list:
-    list_total += item/len(b_list)
-print(list_total)
-
-for item in c_list:
-    list_total += item/len(c_list)
-print(list_total)
+# '''
+# a_list = [3,12,3,5,3,4,6,8,5,3,5,6,3,2,4]
+# b_list = [4,15,2,7,8,3,1,10,9]
+# c_list = [5,10,13,12,5,9,2,6,1,8,8,9,11,13,14,8,2,2,6,3,9,8,10]
+#
+# list_total = 0
+#
+# for item in a_list:
+#     list_total += item/len(a_list)
+# print(list_total)
+#
+# for item in b_list:
+#     list_total += item/len(b_list)
+# print(list_total)
+#
+# for item in c_list:
+#     list_total += item/len(c_list)
+# print(list_total)
 
 '''
 2.) Write a program that will strip the username (whatever is in front of the @ symbol)
 from any e-mail address and print it. First ask the user for their e-mail address.
 '''
+# email = input("Hi, Please input your email here : ")
 
 
 
@@ -43,9 +44,12 @@ TEXT FORMATTING:
      Do not use any plus sign (+) in your code.
      You should only have two double quotes in each print statement.
      '''
-# score = 41237
-# highscore = 1023407
-# print("Score:      " + str(score) )
-# print("High score: " + str(highscore) )
+score = 41237
+highscore = 1023407
+print("Score:      " + "{:,}".format(score))
+print("High score: " + "{:,}".format(highscore) )
+
+
+
 
 
