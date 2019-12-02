3afb30862b57e9aa17420b2ac24d7da72dc4f994
leavertonnel <leavertonnel@urbandaleschools.com>
2019-12-02 14:22:42 -0600

    just a commit

--- 8.0_Jedi_Training.py
+++ 8.0_Jedi_Training.py
@@ -8,19 +8,19 @@ Don't use that. Sum the numbers individually as shown in the chapter.
 Also, a common mistake is to calculate the average each time through the loop 
 to add the numbers. Finish adding the numbers before you divide.
 # '''
-a_list = [3,12,3,5,3,4,6,8,5,3,5,6,3,2,4]
-b_list = [4,15,2,7,8,3,1,10,9]
-c_list = [5,10,13,12,5,9,2,6,1,8,8,9,11,13,14,8,2,2,6,3,9,8,10]
+# a_list = [3,12,3,5,3,4,6,8,5,3,5,6,3,2,4]
+# b_list = [4,15,2,7,8,3,1,10,9]
+# c_list = [5,10,13,12,5,9,2,6,1,8,8,9,11,13,14,8,2,2,6,3,9,8,10]
+#
+# total = 0
+# average = total/len(num)
+#
+# my_list = [a_list, b_list, c_list]
+# num = [a_list, b_list, c_list]
 
-total = 0
-# average = total/len(mun)
-
-my_list = [a_list, b_list, c_list]
-mun = [a_list, b_list, c_list]
-
-for my_list in range(3):
-    total += my_list
-    print(total/len(mun))
+# for my_list in range(3):
+#     total += my_list
+#     print(total/len(mun))
 
 # for item in my_list:
 #     total+=item
@@ -32,7 +32,6 @@ from any e-mail address and print it. First ask the user for their e-mail addres
 '''
 # email = input("Hi, Please input your email here : ")
 # print(email[0:email.find("@")])
-
 '''
 TEXT FORMATTING:
 3.) Make following program output the following:
