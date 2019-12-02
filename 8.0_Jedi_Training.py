c1f81f99b95097355ba0abb7cf4be02219800e01
leavertonnel <leavertonnel@urbandaleschools.com>
2019-12-02 13:52:34 -0600

    just a commit

--- 8.0_Jedi_Training.py
+++ 8.0_Jedi_Training.py
@@ -8,19 +8,19 @@ Don't use that. Sum the numbers individually as shown in the chapter.
 Also, a common mistake is to calculate the average each time through the loop 
 to add the numbers. Finish adding the numbers before you divide.
 # '''
-a_list = [3,12,3,5,3,4,6,8,5,3,5,6,3,2,4]
-b_list = [4,15,2,7,8,3,1,10,9]
-c_list = [5,10,13,12,5,9,2,6,1,8,8,9,11,13,14,8,2,2,6,3,9,8,10]
-
-total = 0
-# average = total/len(mun)
-
-my_list = [a_list, b_list, c_list]
-mun = [a_list, b_list, c_list]
-
-for my_list in range(3):
-    total += my_list
-    print(total/len(mun))
+# a_list = [3,12,3,5,3,4,6,8,5,3,5,6,3,2,4]
+# b_list = [4,15,2,7,8,3,1,10,9]
+# c_list = [5,10,13,12,5,9,2,6,1,8,8,9,11,13,14,8,2,2,6,3,9,8,10]
+#
+# total = 0
+# # average = total/len(mun)
+#
+# my_list = [a_list, b_list, c_list]
+# mun = [a_list, b_list, c_list]
+#
+# for my_list in range(3):
+#     total += my_list
+#     print(total/len(mun))
 
 # for item in my_list:
 #     total+=item
@@ -31,7 +31,6 @@ for my_list in range(3):
 from any e-mail address and print it. First ask the user for their e-mail address.
 '''
 # email = input("Hi, Please input your email here : ")
-#
 # print(email[0:email.find("@")])
 
 '''
@@ -44,11 +43,10 @@ TEXT FORMATTING:
      Do not use any plus sign (+) in your code.
      You should only have two double quotes in each print statement.
      '''
-# score = 41237
-# highscore = 1023407
-# print("Score:      " + "{:,}".format(score))
-# print("High score: " + "{:,}".format(highscore) )
-
+score = 41237
+highscore = 1023407
+print(f"Score: {score:,}")
+print(f"High score: {highscore:,}")
 
 
 
