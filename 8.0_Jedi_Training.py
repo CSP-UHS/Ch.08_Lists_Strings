6990ba1340ff4b3bc7075db0cea30bde3989b5ac
barleypeg <barleypeg@urbandaleschools.com>
2019-12-03 14:23:59 -0600

    ch. 7

--- 8.0_Jedi_Training.py
+++ 8.0_Jedi_Training.py
@@ -8,9 +8,10 @@ Don't use that. Sum the numbers individually as shown in the chapter.
 Also, a common mistake is to calculate the average each time through the loop 
 to add the numbers. Finish adding the numbers before you divide.
 '''
-a_list = [3,12,3,5,3,4,6,8,5,3,5,6,3,2,4]
-b_list = [4,15,2,7,8,3,1,10,9]
-c_list = [5,10,13,12,5,9,2,6,1,8,8,9,11,13,14,8,2,2,6,3,9,8,10]
+
+# a_list = [3,12,3,5,3,4,6,8,5,3,5,6,3,2,4]
+# b_list = [4,15,2,7,8,3,1,10,9]
+# c_list = [5,10,13,12,5,9,2,6,1,8,8,9,11,13,14,8,2,2,6,3,9,8,10]
 
 
 
@@ -19,21 +20,30 @@ c_list = [5,10,13,12,5,9,2,6,1,8,8,9,11,13,14,8,2,2,6,3,9,8,10]
 from any e-mail address and print it. First ask the user for their e-mail address.
 '''
 
-
+while True:
+    email = input("What is your email address?")
+    username = ""
+    for item in email:
+        if item != "@":
+            break
+        username += item
+    print(username)
 
 '''
 TEXT FORMATTING:
 3.) Make following program output the following:
-     
+
+
      Score:          41,237
      High score:  1,023,407
      
      Do not use any plus sign (+) in your code.
      You should only have two double quotes in each print statement.
-     '''
-score = 41237
-highscore = 1023407
-print("Score:      " + str(score) )
-print("High score: " + str(highscore) )
+ '''
+
+# score = 41237
+# highscore = 1023407
+# print("Score:      " + str(score) )
+# print("High score: " + str(highscore) )
 
 
