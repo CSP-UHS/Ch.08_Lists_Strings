a344e6adff27e864b91208f812ce60900c5c4191
halsteaddan <halsteaddan@urbandaleschools.com>
2019-12-03 13:56:41 -0600

    Chapter 8 Jedi Training.

--- 8.1_Months.py
+++ 8.1_Months.py
@@ -1,7 +1,7 @@
 '''
 MONTHS PROGRAM
 --------------
-Write a user-input statement where a user enters a month number 1-13.
+Write a user-input statement where a user enters a month number 1-12.
 Using the starting string below in your program, print the three month abbreviation 
 for the month number that the user enters. Keep repeating this until the user enters 13 to quit.
 Once the user quits, print "Goodbye!"
@@ -9,3 +9,11 @@ Once the user quits, print "Goodbye!"
 months = "JanFebMarAprMayJunJulAugSepOctNovDec"
 
 '''
+
+months = "JanFebMarAprMayJunJulAugSepOctNovDec"
+
+while True:
+    UserInput=int(input("Pick a month number (1-12):"))
+    if UserInput<=1 or UserInput>=12:
+        break
+print("Goodbye.")
