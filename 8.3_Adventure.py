78da7ba1c8b5acb3cb6989d0382f0b7ea2345de9
leavertonnel <leavertonnel@urbandaleschools.com>
2019-12-05 14:31:26 -0600

    just a commit

--- 8.3_Adventure.py
+++ 8.3_Adventure.py
@@ -8,16 +8,16 @@ ADVENTURE PROGRAM
 '''
 room_list = [] #empty ray of room list
 
-room = ["Welcome to the front porch Detective, we need you to look at the body quickly! Go find the body", 2, None, None, None]   #THE COPY
-room_list.append(room) #THE COPY
-
-room = ["You are now in the South Hall", 5, 3, 0, 1]   #THE COPY
+room = ["Welcome to the front porch Detective. You have finally made it! We need you to look at the body quickly! Go find the body", 2, None, None, None]   #THE COPY
 room_list.append(room) #THE COPY
 
 room = ["You are now in the 2 Bedroom", 4, 2, None, None]   #THE COPY
 room_list.append(room) #THE COPY
 
 room = ["You are now in the South Hall", 5, 3, 0, 1]   #THE COPY
+room_list.append(room) #THE COPY
+
+room = ["You are now in the Dining room, but smell somthing odd...wait is that blood?", 6, None, None, 2]   #THE COPY
 room_list.append(room) #THE COPY
 
 room = ["You are now in the grand bedroom", None, 5, 1, None]   #Bed room 1
@@ -29,35 +29,41 @@ room_list.append(room) #THE COPY
 room = ["You have walked into the kitchen to find blood splatter every where. The body lays on the floor face downwards a knife stuck in the cospes back", None, None, 3, 5]   #THE COPY
 room_list.append(room) #THE COPY
 
-room = ["You are now in the Dining room, but smell somthing odd...wait is that blood?", 6, None, None, 2]   #THE COPY
+room = ["You have walked onto the balcony", None, None, 5, None]   #THE COPY
 room_list.append(room) #THE COPY
 
 current_room = 0
 
 done = False
 
-while False:
+while done == False:
+    print(room_list[current_room][0])
     print()
-    room_list[current_room][0]
-    print("Hello Detective! You have finally made it! I was worried you were lost, Please come and take a look at the body")
     direction =input("What direction would you like to go? N, E, S, W or Q for quit: ")
     if direction.lower() == "north" or direction.lower() == "n":
         next_room = room_list[current_room][1]
-
+        if next_room == None:
+            print("You cant fo that way")
+        else:
+            current_room = next_room
     elif direction.lower() == "east" or direction.lower() == "e":
-        next_room = room_list[current_room][1]
-
+        next_room = room_list[current_room][2]
+        if next_room == None:
+            print("You cant fo that way")
+        else:
+            current_room = next_room
     elif direction.lower() == "south" or direction.lower() == "s":
-        next_room = room_list[current_room][1]
-
+        next_room = room_list[current_room][3]
+        if next_room == None:
+            print("You cant fo that way")
+        else:
+            current_room = next_room
     elif direction.lower() == "west" or direction.lower() == "w":
-        next_room = room_list[current_room][1]
-
-    if next_room == None:
-        print("You cant fo that way")
-    else:
-        current_room = next_room
-
+        next_room = room_list[current_room][4]
+        if next_room == None:
+            print("You cant fo that way")
+        else:
+            current_room = next_room
     if direction.lower() == "q":
         done = True
 
