c17ffc1c042f65b9a1d321ef667ffa31a871ab60
leavertonnel <leavertonnel@urbandaleschools.com>
2019-12-05 15:30:11 -0600

    just a commit ;)

--- 8.3_Adventure.py
+++ 8.3_Adventure.py
@@ -8,35 +8,35 @@ ADVENTURE PROGRAM
 '''
 room_list = []
 
-room = ["Welcome to the front porch Detective. You have finally made it! We need you to look at the body quickly! Go find the body", 1, None, None, None]
+room = ["\nWelcome to the front porch Detective. You have finally made it! We need you to look at the body quickly! Go find the corpse.", 1, None, None, None]
 room_list.append(room)
 
-room = ["You walk into Ballroom, the walls are a bright gold, with a grand piano, and a rose couch", None, 9, 0, 2]
+room = ["You walk into the Ballroom, the walls are a bright gold. A grand piano sits on the left side of the room and a rose couch on the right.", None, 9, 0, 2]
 room_list.append(room)
 
-room = ["You are now in the Conservatory, its a quit room. There are some plants and a small desk. You'll be suprised what you find ", 3, None, 2, None]
+room = ["You are now in the Conservatory, its a quiet room. There are some plants and a small desk. You'll be suprised what you'll find if you look around. ", 3, None, 2, None]
 room_list.append(room)
 
-room = ["You are now in the Billard room, nothing here but a pool table", 4, None, 2, None]
+room = ["You are now in the Billard's room, nothing here but a pool table.", 4, None, 2, None]
 room_list.append(room)
 
-room = ["You are now in the Library, a big table sits in the middle of the room. The walls are lined with books", 5, None, 4, None]
+room = ["In the Library, a big table sits in the middle of the room. The walls are lined with books", 5, None, 4, None]
 room_list.append(room)
 
-room = ["The Study is peacefull, there is a purple couch and a desk with a lamp. ", None, 6, 4, None]
+room = ["The Study is peaceful, looking around you see a purple couch with a desk and a lamp. ", None, 6, 4, None]
 room_list.append(room)
 
-room = ["You are now in the Hall, nothing here but a small round table in the middle of the room.", None, 7, None, 5]   #
+room = ["You are now in the Hall, nothing here but a small round table in the middle of the room. The wall are a bland cream, and floor is wooden. nothing here", None, 7, None, 5]   #
 room_list.append(room)
 
-room = ["You are now in the Lounge, green couches line the room, the wall is a bland color but the carpet is a bold blue", None, None, 8, 6]
+room = ["You are now in the Lounge, Green couches line the room. The wall's are a bland color but the carpet is a bold blue", None, None, 8, 6]
 room_list.append(room)
 
-room = ["You are now in the Dining room, wood floors and bland walls...but you smell something odd...wait is that blood?", 7, None, 9, None]
+room = ["Walking into the Dining room, you see wood floors and a grand wooden table...but you smell something odd...wait is that blood?", 7, None, 9, None]
 room_list.append(room)
 
-room = ["You have walked in the Kitchen, blood lines the floor. Splattered in every place possible, you can barley see the light blue walls in all the red.\n"
-        "But what happed to the body....", 8, None, None, 1]
+room = ["You make it into the Kitchen, blood lines the floor, splattered in every place possible. You can barley see the light blue walls in all the red.\n"
+        "But what happened to the body....", 8, None, None, 1]
 room_list.append(room)
 
 current_room = 0
