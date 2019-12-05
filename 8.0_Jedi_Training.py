e7c5a65b6391d881ba544bb58ed0071a7cdfbfd1
watsoncal <watsoncal@urbandaleschools.com>
2019-12-05 14:00:56 -0600

    8.0 Jedi Training Complete

--- 8.0_Jedi_Training.py
+++ 8.0_Jedi_Training.py
@@ -1,4 +1,4 @@
-#Sign your name:________________
+#Sign your name: Cal Watson
 
 '''
 1.)
@@ -12,15 +12,32 @@ a_list = [3,12,3,5,3,4,6,8,5,3,5,6,3,2,4]
 b_list = [4,15,2,7,8,3,1,10,9]
 c_list = [5,10,13,12,5,9,2,6,1,8,8,9,11,13,14,8,2,2,6,3,9,8,10]
 
-
+list_total = 0
+user_choice = input("Which list would you like to find the average for? A. a_list B. b_list C. c_list ")
+if user_choice.upper() == "A":
+    user_choice = a_list
+elif user_choice.upper() == "B":
+    user_choice = b_list
+elif user_choice.upper() == "C":
+    user_choice = c_list
+for item in user_choice:
+    list_total+=item
+average = list_total/len(user_choice)
+print(average)
 
 '''
 2.) Write a program that will strip the username (whatever is in front of the @ symbol)
 from any e-mail address and print it. First ask the user for their e-mail address.
 '''
-
-
-
+while True:
+    email=input("What is your email address? ")
+    count=0
+    position=0
+    for item in email:
+        if item=="@":
+            position=count
+        count+=1
+    print(email[0:position])
 '''
 TEXT FORMATTING:
 3.) Make following program output the following:
@@ -33,7 +50,7 @@ TEXT FORMATTING:
      '''
 score = 41237
 highscore = 1023407
-print("Score:      " + str(score) )
-print("High score: " + str(highscore) )
+print(f"Score:      {score:>9,d}")
+print(f"High score: {highscore:,}")
 
 
