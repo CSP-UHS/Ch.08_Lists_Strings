5e053233213ff3477487e44b19f9dae3b15cac0f
halsteaddan <halsteaddan@urbandaleschools.com>
2019-12-09 14:07:43 -0600

    Chapter 8 Jedi Training.

--- 8.0_Jedi_Training.py
+++ 8.0_Jedi_Training.py
@@ -12,21 +12,24 @@ a_list = [3,12,3,5,3,4,6,8,5,3,5,6,3,2,4]
 b_list = [4,15,2,7,8,3,1,10,9]
 c_list = [5,10,13,12,5,9,2,6,1,8,8,9,11,13,14,8,2,2,6,3,9,8,10]
 
-
+total = 0
+for item in a_list:
+    total += item
+average = total / len(a_list)
+print(average)
 
 '''
 2.) Write a program that will strip the username (whatever is in front of the @ symbol)
 from any e-mail address and print it. First ask the user for their e-mail address.
 '''
-Done = False
-while not Done:
-    Email=input("What is your email address?")
-    Username=""
-    for item in Email:
-        if item == "@":
-            break
-        Username+=item
-    print(Username)
+
+user_username = ""
+user_email = str(input("What is your email address?"))
+for item in user_email:
+    if item == "@":
+        break
+    user_username += item
+print("Your username is", user_username  + ".")
 
 '''
 TEXT FORMATTING:
@@ -38,9 +41,3 @@ TEXT FORMATTING:
      Do not use any plus sign (+) in your code.
      You should only have two double quotes in each print statement.
      '''
-score = 41237
-highscore = 1023407
-print("Score:      " + str(score) )
-print("High score: " + str(highscore) )
-
-
