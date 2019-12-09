5f81c73b4034510556f2190284504c36d97ad03c
watsoncal <watsoncal@urbandaleschools.com>
2019-12-09 14:06:22 -0600

    Finished Decryption

--- 8.2_Decryption.py
+++ 8.2_Decryption.py
@@ -6,8 +6,14 @@ of the string into its ASCII decimal number, applied a +/-20 algorithm to it and
 characters. Your task is to write a Decryption program to decipher the following secret code. Don't waste time changing 
 your program 40 times. Use a FOR loop from -20 to +20 to generate all the possibilities in one run of your program.
 
-Secret Message: Lxwp{j}~uj}rxw|*)bx~)l{jltnm)}qn)lxmn7)]qn)ox{ln)r|)\][XWP)r}q)x~*
+Secret Message: Lxwp{j}~uj}rxw|*)bx~)l{jltnm)}qn)lxmn7)]qn)ox{ln)r|)\][XWP)r}q) x~*
 '''
+secret_message = "Lxwp{j}~uj}rxw|*)bx~)l{jltnm)}qn)lxmn7)]qn)ox{ln)r|)\][XWP)r}q) x~*"
 
-
-
+for j in range(-20,20,1):
+    decrypt=""
+    for i in range(len(secret_message)):
+        num = (ord(secret_message[i]))
+        num+=j
+        decrypt+=(chr(num))
+    print(decrypt)
