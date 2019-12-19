a532ca6c3b76acaf25abd378939dcf351cc6e5c6
alexanderrow <alexanderrow@urbandaleschools.com>
2019-12-19 14:13:18 -0600

    picasso project 2

--- 8.0_Jedi_Training.py
+++ 8.0_Jedi_Training.py
@@ -12,7 +12,7 @@ a_list = [3,12,3,5,3,4,6,8,5,3,5,6,3,2,4]
 b_list = [4,15,2,7,8,3,1,10,9]
 c_list = [5,10,13,12,5,9,2,6,1,8,8,9,11,13,14,8,2,2,6,3,9,8,10]
 
-
+for a_list
 
 '''
 2.) Write a program that will strip the username (whatever is in front of the @ symbol)
