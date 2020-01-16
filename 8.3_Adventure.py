ace625870d774b1ac7f4c2f2b1bd62d3fd8b9785
Danny Halstead <halsteaddan@urbandaleschools.com>
2020-01-16 00:47:08 -0600

    Adventure Game.

--- 8.3_Adventure.py
+++ 8.3_Adventure.py
@@ -7,24 +7,24 @@ ADVENTURE PROGRAM
 '''
 
 room_list = [] #Sets up empty room list to be added to later
-room = ("You have entered the house. From here you must get to the bunker asap. The doorroom has nothing in it.",1, None, None, None) #Doorway
+room = ("You have entered the house. From here you must get to the bunker asap. The door-room has nothing in it.",1, None, None, None) #Doorway
 room_list.append(room) #Adds the above room to list, used for every room
-room = ("This living room has been long abandoned.",None, 2, 0, 3) #Living Room
+room = ("This living room has been long abandoned. The hallway to your left (west) looks promising.",None, 2, 0, 3) #Living Room
 room_list.append(room)
 room = ("The sunroom is filled with dirt.",5, None, None, 1) #Sunroom
 room_list.append(room)
 room = ("The twisted hallway leads to a heavy locked door that will require a key.",4, 1, None, None) #Hallway
 room_list.append(room)
-room = ("In keeping with most of the house, there is nothing of interest in this foyer..",6, None, 3, None) #Foyer
+room = ("You unlock the door and keep moving forward. In keeping with most of the house, there is nothing of interest in this foyer.",6, None, 3, None) #Foyer
 room_list.append(room)
 room = ("This dark room has a skeleton in the corner, its mouth appears to be holding something, you examine it and find a key.",None, None, 2, None) #Darkroom
 #Decided to automaticly give the player a key instead of forcing them to interact with a skelton because who wants to interact with a skeleton?
 room_list.append(room)
-room = ("Congratulations, you reached the bunker and are now safe!",None, None, 4, None) #Bunker
+room = ("Due to the horrible way I programmed this game, this text will never appear.",None, None, 4, None) #Bunker
 room_list.append(room)
 
 print("World War 3 has started and America is being attacked.") #Establishes a beleivable story to get the player emotionally invested
-print("Your freinds house has a nuclear bunker and they are out of town so you decide to go there.")
+print("Your friend's house has a nuclear bunker and they are out of town so you decide to go there.")
 print("Unluckily for you, their house is not very straightforward.")
 
 current_room = 0 #Starting Location as doorway
@@ -45,6 +45,10 @@ while not done: #Makes program run until stopped
        if current_room == 2: #Again really bad way to check if the player is in a certan room to give them a key, my really tired brain can't think of a better way lol
            player_has_key = True
        next_room = room_list[current_room][1]
+       if current_room == 4: #checks if the player has won
+            print("You have reached the bunker and are now safe!")
+            print("Thanks for playing.")
+            done = True
 
        if next_room == None:
            print("There is no room in that direction, try a different way.")
@@ -68,7 +72,7 @@ while not done: #Makes program run until stopped
            print("There is no room in that direction, try a different way.")
        else: current_room = next_room
 
-    elif user_choice.lower() == "quit" or user_choice.lower() == "q": #allows the player to move quit
+    elif user_choice.lower() == "quit" or user_choice.lower() == "q" or current_room == 6: #allows the player to quit
        print("Thanks for playing.")
        done = True
 
