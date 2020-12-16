1a701e1a876a407868bb66e57c735f60f13d618a
Alex <alexmears2003@gmail.com>
2020-12-16 11:16:42 -0600

    Finished the training

--- 8.0_Jedi_Training.py
+++ 8.0_Jedi_Training.py
@@ -1,4 +1,4 @@
-#Sign your name:________________
+# Sign your name: Alex Mears
 
 '''
 1.)
@@ -8,18 +8,28 @@ Don't use that. Sum the numbers individually as shown in the chapter.
 Also, a common mistake is to calculate the average each time through the loop 
 to add the numbers. Finish adding the numbers before you divide.
 '''
+
 a_list = [3,12,3,5,3,4,6,8,5,3,5,6,3,2,4]
 b_list = [4,15,2,7,8,3,1,10,9]
 c_list = [5,10,13,12,5,9,2,6,1,8,8,9,11,13,14,8,2,2,6,3,9,8,10]
 
-
+tot=0
+for i in range(len(a_list)):
+    tot += a_list[i]
+tot /= len(a_list)
+print(tot)
 
 '''
 2.) Write a program that will strip the username (whatever is in front of the @ symbol)
 from any e-mail address and print it. First ask the user for their e-mail address.
 '''
 
-
+email = input("what is your email?")
+for i in range(len(email)):
+    if email[i] == "@":
+        print("your username is ")
+        print(email[:i])
+        break
 
 '''
 TEXT FORMATTING:
@@ -31,9 +41,9 @@ TEXT FORMATTING:
      Do not use any plus sign (+) in your code.
      You should only have two double quotes in each print statement.
      '''
+
 score = 41237
 highscore = 1023407
-print("Score:      " + str(score) )
-print("High score: " + str(highscore) )
-
+print("Score:        ", str(score))
+print("High score: ", str(highscore))
 
