d121a9bcab467bcfc0d79e957106f99387963b02
Alex <alexmears2003@gmail.com>
2020-12-16 11:12:49 -0600

    Decrypted Hermons message

--- 8.2_Decryption.py
+++ 8.2_Decryption.py
@@ -1,13 +1,21 @@
 '''
 DECRYPTION PROGRAM
 ------------------
-An encryption program was used to generate the following secret code. The encryption program converted each character 
+An encryption program was used to generate the following secret code. The encryption program converted each character
 of the string into its ASCII decimal number, applied a +/-20 algorithm to it and then converted it back to
-characters. Your task is to write a Decryption program to decipher the following secret code. Don't waste time changing 
+characters. Your task is to write a Decryption program to decipher the following secret code. Don't waste time changing
 your program 40 times. Use a FOR loop from -20 to +20 to generate all the possibilities in one run of your program.
 
 Secret Message: Lxwp{j}~uj}rxw|*)bx~)l{jltnm)}qn)lxmn7)]qn)ox{ln)r|)\][XWP)r}q)x~*
 '''
+enctext = 'Lxwp{j}~uj}rxw|*)bx~)l{jltnm)}qn)lxmn7)]qn)ox{ln)r|)\][XWP)r}q)x~*'
+for i in range(-20, 20, 1):
+    text = ""
+    for c in enctext:
+        text += chr(ord(c)-i)
+    print(text + "  " + str(i))
+    continue
 
-
-
+'''
+The encrypted message is 'Congratulations! You cracked the code. The force is STRONG with you!' the key was +9
+'''
