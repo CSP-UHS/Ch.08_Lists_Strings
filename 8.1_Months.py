8040555a1a6de53b021707f6df1f26b8508eeab0
thonathan <januszewskieth@urbandaleschools.com>
2021-01-15 12:57:33 -0600

    ch8 stuff

--- 8.1_Months.py
+++ 8.1_Months.py
@@ -9,3 +9,13 @@ Once the user quits, print "Goodbye!"
 months = "JanFebMarAprMayJunJulAugSepOctNovDec"
 
 '''
+months = "JanFebMarAprMayJunJulAugSepOctNovDec"
+
+while True:
+    ans=int(input("Please enter a number 1-13: "))
+    if 0 < ans < 13:
+        print(months[(ans - 1) * 3:((ans - 1) * 3) + 3])
+    else:
+        print("Goodbye!")
+        break
+                
