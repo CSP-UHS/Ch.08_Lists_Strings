cc253859911e5ad277858872727d750ccfc9bc39
redeniuseva <redeniuseva@urbandaleschools.com>
2021-01-18 14:46:27 -0600

    Finished Months

--- 8.1_Months.py
+++ 8.1_Months.py
@@ -9,3 +9,45 @@ Once the user quits, print "Goodbye!"
 months = "JanFebMarAprMayJunJulAugSepOctNovDec"
 
 '''
+
+per = input("enter a month number 1-12: ")
+
+months = ["Jan ","Feb ","Mar ","Apr ","May ","Jun ","Jul ","Aug ","Sep ","Oct ","Nov ","Dec "]
+
+if per == 1:
+    print(months[0])
+
+elif per == 2:
+    print(months[1])
+
+elif per == 3:
+    print(months[2])
+
+elif per == 4:
+    print(months[3])
+
+elif per == 5:
+    print(months[4])
+
+elif per == 6:
+    print(months[5])
+
+elif per == 7:
+    print(months[6])
+
+elif per == 8:
+    print(months[7])
+
+elif per == 9:
+    print(months[8])
+
+elif per == 10:
+    print(months[9])
+
+elif per == 11:
+    print(months[10])
+
+elif per == 12:
+    print(months[11])
+
+
