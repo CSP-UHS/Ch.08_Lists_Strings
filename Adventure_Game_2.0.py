1849f1d03ec6cb1c711cb7a5f8a2099ff035f5db
schmidtjos <schmidtjos@urbandaleschools.com>
2021-02-02 10:18:32 -0600

    finished 9.2

--- Adventure_Game_2.0.py
+++ Adventure_Game_2.0.py
@@ -1,32 +1,21 @@
-# x = 2
-# def program():
-#     while x <= 12:
-#         print("Hello")
-#         answer = input("num: ")
-#         if answer.isnumeric():
-#             x = int(answer)
-#         else:
-#             print("Dumbass")
-#
-# program()
-#
-# x = 2
-# game = True
-#
-# def dood():
-#     global game, x
-#     print(x)
-#     while game:
-#         answer = input("num")
-#         if answer.isnumeric():
-#             x = int(answer)
-#             print(x + 20)
-#         else:
-#             game = False
-#
-#
-# dood()
+import random
 
+class Player():
+    def __init__(self):
+        self.ac = 16
+        self.hp = 10
+        self.stats = ["Str", "Con", "Dex", "Wis", "Int", "Cha"]
+        self.weapon = "Fists"
+        self.shield = None
+        self.inventory = []
 
-
-
+def pseudopod():
+    attack = random.randint(1, 21) + 3
+    if attack >= Player.hp:
+        damage = (random.randint(1, 7) + 1) + random.randint(1, 7) + random.randint(1, 7)
+class Slime():
+    def __init__(self):
+        self.ac = 8
+        self.hp = 22
+        self.stats = [12, 6, 16, 1, 6, 2]
+        self.attack = [pseudopod()]
