dbca6e927c87e47f607a97d74a96e1ef8d9efe29
mitchelldan <mitchelldan@urbandaleschools.com>
2021-02-18 13:01:03 -0600

    did thing

--- 8.3_Adventure.py
+++ 8.3_Adventure.py
@@ -5,4 +5,16 @@ ADVENTURE PROGRAM
 2.) Print off a physical map for players to use with your program
 3.) Expand your program to make it a real adventure game
 
+
+IDEAS:
+ -TIME TRAVEL, Same levels across 3 differnt stages in time "Past/Present/Future"
+ -PERSPECTIVE WARP, A item that lets you change your perspective (ex: Pickup specal item to allow you to change the size of a door)
+
 '''
+#This is where my adventure game would go, IF I HAD ONE
+
+
+room_list = []
+#0
+room = ["You are in the Court Yard. Its a rectangular grassy area with a large tree in the center. On all sides the Courtyard is surrounded by blackballs with decoration that reminds you of the 1920s. On one wall there are 3 doors, The door furthest to the left is ridiculously large. The door handle is at least 15 feet off the ground. The door in the middle is normal sized. The door furthest to the right is very small, perhaps large enought for you to put your hand in. "]
+room_list.append(room)
