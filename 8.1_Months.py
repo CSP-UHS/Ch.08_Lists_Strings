0aa64a799c3b7b94179acb95566c3a506de5b4a3
bakerkai <bakerkai@urbandaleschools.com>
2021-11-23 10:42:41 -0600

    this is too hard i give up

--- 8.1_Months.py
+++ 8.1_Months.py
@@ -8,5 +8,26 @@ Once the user quits, print "Goodbye!"
 
 months = "JanFebMarAprMayJunJulAugSepOctNovDec"
 
+months = "JanFebMarAprMayJunJulAugSepOctNovDec"
+
+print("Please select a month that you want the name for using the 1 - 12")
+first = int(last - 3)
+last = usernum
+usernum = int(input("What is your number"))
+month = [first:last]
+
+num = input("What is your number? (1 - 12)")
+
+last = int(num * 3)
+first = int(last - 3)
+
+print(first, last)
 '''
 months = "JanFebMarAprMayJunJulAugSepOctNovDec"
+done = False
+while not done:
+    usermonth = int(input("What is your number? (1 - 12)"))
+    q = str(input("Is this all you need?"))
+    print[first:last]
+    if q == 'Y':
+        done = True
