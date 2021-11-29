3cb5054eb3fd1e3cb5dd90cdb9ce0be36f7d2af8
jacobsonwil <jacobsonwil@urbandaleschools.com>
2021-11-29 09:59:39 -0600

    ch 8 commit

--- 8.1_Months.py
+++ 8.1_Months.py
@@ -14,10 +14,10 @@ months = "JanFebMarAprMayJunJulAugSepOctNovDec"
 done = False
 
 while not done:
-    num = int(input("What month? To quit break range of 1-12 "))
+    num = int(input("Chose a month 1-12 or 13 to quit: "))
     end = num * 3
     start = end - 3
-    if num >= 1 and num <= 12:
-        print(months[start,end])
-    else:
+    if num == 13:
         done = True
+    else:
+        print("Your month was ", months[start:end])
