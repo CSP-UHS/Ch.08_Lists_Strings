e0a2be020a0ff3b4e384bc38125c50c2bc0ab6ec
toricden <toricden@urbandaleschools.com>
2021-11-30 11:03:18 -0600

    Save 19

--- 8.2_Decryption.py
+++ 8.2_Decryption.py
@@ -8,6 +8,19 @@ your program 40 times. Use a FOR loop from -20 to +20 to generate all the possib
 
 Secret Message: Lxwp{j}~uj}rxw|*)bx~)l{jltnm)}qn)lxmn7)]qn)ox{ln)r|)\][XWP)r}q)x~*
 '''
+Secret_Message="Lxwp{j}~uj}rxw|*)bx~)l{jltnm)}qn)lxmn7)]qn)ox{ln)r|)\][XWP)r}q)x~*"
 
-
+check=len(Secret_Message)
+for i in range(-20,21,1):
+    decrypt=""
+    for c in Secret_Message:
+        id=ord(c)
+        id+=i
+        ch = chr(id)
+        decrypt += ch
+        if id==32 or id==33 or id==46 or 65<=id<=90 or 97<=id<=122:
+            check-=1
+        if check==0:
+            break
+print(i,":",decrypt)
 
--- 8.3_Adventure.py
+++ 8.3_Adventure.py
@@ -6,3 +6,60 @@ ADVENTURE PROGRAM
 3.) Expand your program to make it a real adventure game
 
 '''
+room_list=[]
+#create room list
+room=["You are on the front porch",2,None,None,None]
+room_list.append(room)
+room=["You are in Bedroom 2",4,2,None,None]
+room_list.append(room)
+room=["You are in the South Hall",5,3,0,1]
+room_list.append(room)
+room=["You are in the dining room",6,None,None,2]
+room_list.append(room)
+
+current_room=0
+done=False
+while not done:
+    print()
+    print(room_list[current_room][0])
+    bing=input("Type N,E,S,W for directions or Q to rage quit")
+
+    #NORTH
+    if bing.lower==("n") or bing.lower==("north"):
+        nextroom=room_list[current_room][1]
+    if nextroom== None:
+        print("You cant go there dummy")
+    else:
+        current_room=nextroom
+
+        # EAST
+    elif bing.lower==("e") or bing.lower==("east"):
+        nextroom = room_list[current_room][2]
+    if nextroom == None:
+        print("You cant go there dummy")
+    else:
+        current_room = nextroom
+
+        # SOUTH
+    elif bing.lower == ("s") or bing.lower == ("south"):
+        nextroom = room_list[current_room][3]
+    if nextroom == None:
+        print("You cant go there dummy")
+    else:
+        current_room = nextroom
+
+        # WEST
+    elif bing.lower == ("w") or bing.lower == ("west"):
+    nextroom = room_list[current_room][4]
+    if nextroom == None:
+    print("You cant go there dummy")
+    else:
+        current_room = nextroom
+
+        #QUIT
+
+    elif bing.lower == ("q") or bing.lower == ("quit"):
+    done=True
+
+    #stupid
+    else: print("Invalid answer bud")
