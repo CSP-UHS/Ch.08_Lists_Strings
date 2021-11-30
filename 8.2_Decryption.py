bcd1d0600966835d5af1fc6d343e28e31cc2063c
bakerkai <bakerkai@urbandaleschools.com>
2021-11-30 10:38:21 -0600

    eeee

--- 8.2_Decryption.py
+++ 8.2_Decryption.py
@@ -8,6 +8,20 @@ your program 40 times. Use a FOR loop from -20 to +20 to generate all the possib
 
 Secret Message: Lxwp{j}~uj}rxw|*)bx~)l{jltnm)}qn)lxmn7)]qn)ox{ln)r|)\][XWP)r}q)x~*
 '''
+secretmsg = "Lxwp{j}~uj}rxw|*)bx~)l{jltnm)}qn)lxmn7)]qn)ox{ln)r|)\][XWP)r}q)x~*"
 
+for i in range(-20, 21, 1):
+    check = len(secretmsg)
+    decrypt=""
+    for c in secretmsg:
+        id = ord(c)
+        id += i
+        ch = chr(id)
+        decrypt += ch
+        if id == 32 or id == 33 or id == 46 or 65 <= id <=90 or 97 <= 122:
+            check -= 1
+        if check == 0:
+            break
+    print(i, " : ", decrypt)
 
-
+# secret image is -9  :  Congratulations! You cracked the code. The force is STRONG with you!
