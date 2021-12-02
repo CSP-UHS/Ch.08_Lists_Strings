baecd50536c6287a6f69467adc475dc70f0491d6
fuchswil <fuchswil@urbandaleschools.com>
2021-12-02 09:34:17 -0600

    8.0, 8.1, 8.2 done

--- 8.0_Jedi_Training.py
+++ 8.0_Jedi_Training.py
@@ -8,18 +8,29 @@ Don't use that. Sum the numbers individually as shown in the chapter.
 Also, a common mistake is to calculate the average each time through the loop 
 to add the numbers. Finish adding the numbers before you divide.
 '''
-a_list = [3,12,3,5,3,4,6,8,5,3,5,6,3,2,4]
-b_list = [4,15,2,7,8,3,1,10,9]
-c_list = [5,10,13,12,5,9,2,6,1,8,8,9,11,13,14,8,2,2,6,3,9,8,10]
-
+# a_list = [3,12,3,5,3,4,6,8,5,3,5,6,3,2,4] #4.8
+# b_list = [4,15,2,7,8,3,1,10,9] #6.56
+# c_list = [5,10,13,12,5,9,2,6,1,8,8,9,11,13,14,8,2,2,6,3,9,8,10] #7.57
+# list = c_list
+# total = 0
+# for item in list:
+#     total += item
+# average = total/len(list)
+# print(f'The average is {average:.3}')
 
 
 '''
 2.) Write a program that will strip the username (whatever is in front of the @ symbol)
 from any e-mail address and print it. First ask the user for their e-mail address.
 '''
-
-
+# email = input("Enter your email address: ")
+# username = ""
+# for chr in email:
+#     if chr == "@":
+#         break
+#     else:
+#         username += chr
+# print("Your username is :", username)
 
 '''
 TEXT FORMATTING:
@@ -33,7 +44,6 @@ TEXT FORMATTING:
      '''
 score = 41237
 highscore = 1023407
-print("Score:      " + str(score) )
-print("High score: " + str(highscore) )
-
+print(f"Score: {score:>14,}")
+print(f"High score: {highscore:9,}")
 
--- 8.3_Adventure.py
+++ 8.3_Adventure.py
@@ -6,3 +6,57 @@ ADVENTURE PROGRAM
 3.) Expand your program to make it a real adventure game
 
 '''
+room_list = []
+# Create Rooms and add to room list
+room = ["You are on the front porch.", 2, None, None, None]
+room_list.append(room)
+room = ["You are in Bedroom 2", 4, 2, None, None]
+room_list.append(room)
+room = ["You are in the South Hall", 5, 3, 0, 1]
+room_list.append(room)
+room = ["You are in the Dining Room", 6, None, None, 2]
+room_list.append(room)
+
+current_room = 0
+done = False
+while not done:
+    print()
+    print(room_list[current_room][0])
+    bing = input("Type N, E, W, S, direction or Q to quit\n")
+
+    # NORTH
+    if bing.lower() == "n" or bing.lower() == "north":
+        next_room = room_list[current_room][1]
+        if next_room == None:
+            print("You cannot go that way")
+        else:
+            current_room = next_room
+    # EAST
+    if bing.lower() == "e" or bing.lower() == "east":
+        next_room = room_list[current_room][2]
+        if next_room == None:
+            print("You cannot go that way")
+        else:
+            current_room = next_room
+    # SOUTH
+    if bing.lower() == "s" or bing.lower() == "south":
+        next_room = room_list[current_room][3]
+        if next_room == None:
+            print("You cannot go that way")
+        else:
+            current_room = next_room
+    # WEST
+    if bing.lower() == "w" or bing.lower() == "west":
+        next_room = room_list[current_room][4]
+        if next_room == None:
+            print("You cannot go that way")
+        else:
+            current_room = next_room
+    #QUIT
+    elif bing.lower() == "q" or bing.lower() == "quit":
+        done = True
+    #STUPID
+    # else:
+    #     print("You are stupid!!!!!!!!1 You don't understand what you are doing!!!!")
+
+print("Thanks for playing!")
