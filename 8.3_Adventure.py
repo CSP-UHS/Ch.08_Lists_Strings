e8854e91551bbd95f198892bdeb26ddf32bd6fe5
bakerkai <bakerkai@urbandaleschools.com>
2021-12-02 10:29:33 -0600

    adventure game rooms somewhat sorted out

--- 8.3_Adventure.py
+++ 8.3_Adventure.py
@@ -6,19 +6,24 @@ ADVENTURE PROGRAM
 3.) Expand your program to make it a real adventure game
 
 '''
-
-
+print("Welcome to sneaking into your kitchen simulator!")
+print("You are thirsty at 3 AM and everybody in your house is sleeping")
+print("You must find a way to get into the kitchen and drink the sacred ice juice")
+print("Waking anybody up will fail you, god help you if you wake up h̷͚͝ì̴̝m̷̼͝")
 
 room_list = []
+item_list = []
 # Create rooms and add to list
 
-room = ["You are on the front porch,",2,None, None, None]
+room = ["You are in your bedroom",None, None, None, None]
 room_list.append(room)
-room = ["two,",4,2, None, None]
+room = ["Bedroom 2",1, None, None, None]
 room_list.append(room)
-room = ["North,",5,3,0,1,]
+room = ["Bedroom 3",None,None,None,None]
 room_list.append(room)
-room = ["East,",6, None, None , 2]
+room = ["Bedroom 4",None , None, None , None]
+room_list.append(room)
+room = ["Bathroom",None, None, None , None]
 room_list.append(room)
 
 current_room = 0
@@ -27,39 +32,57 @@ done = False
 while not done:
     print()
     print(room_list[current_room][0])
-    bing = input("Type N, E, W, S directions or Q to quit")
+    bing = input("Type N, E, W, S, P for backpack, Q to quit or S to search")
 
     #NORTH
     if bing.lower()=="n" or bing.lower() == "north":
         next_room = room_list[current_room][1]
         if next_room == None:
-            print("You can't go there dummy")
+            print("You can't go there.")
         else:
             current_room = next_room
     #EAST
     elif bing.lower() == "e" or bing.lower() == "east":
         next_room = room_list[current_room][2]
         if next_room == None:
-            print("You can't go there dummy")
+            print("You can't go there.")
         else:
             current_room = next_room
     #SOUTH
     elif bing.lower() == "s" or bing.lower() == "south":
         next_room = room_list[current_room][3]
         if next_room == None:
-            print("You can't go there dummy")
+            print("You can't go there.")
         else:
             current_room = next_room
     #WEST
     elif bing.lower() == "w" or bing.lower() == "west":
         next_room = room_list[current_room][4]
         if next_room == None:
-            print("You can't go there dummy")
+            print("You can't go there.")
         else:
             current_room = next_room
     #QUIT
-    elif bing.lower() == "Q" or bing.lower() == "quit":
+    elif bing.lower() == "q" or bing.lower() == "quit":
         done = True
+    #POCKET
+    elif bing.lower() == "p" or bing.lower() == "pocket":
+        if len(item_list) == 0:
+            print("You have nothing in your pocket")
+        else:
+            print(item_list)
+    #SEARCH
+    elif bing.lower() == "s" or bing.lower() == "search":
+        if room_list ==
+
     #STUID
     else:
         print("That didn't make any sense, can you actually read please? Or let me know that there is something wrong with my code")
+
+
+#  "\033[1;31;48m" Red
+#  "\033[1;32;48m" Green
+#  "\033[1;33;48m" Yellow
+#  "\033[1;34;48m" Blue
+#  "\033[1;35;48m" Purple
+#  "\033[1;36;48m" Cyan
