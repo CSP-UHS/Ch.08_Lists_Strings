8bfb84949af9d52df44f5b3c82fd65e57fc9ddd6
hubbyada <hubbyada@urbandaleschools.com>
2021-12-02 10:06:40 -0600

    decrypt

--- 8.2_Decryption.py
+++ 8.2_Decryption.py
@@ -8,6 +8,20 @@ your program 40 times. Use a FOR loop from -20 to +20 to generate all the possib
 
 Secret Message: Lxwp{j}~uj}rxw|*)bx~)l{jltnm)}qn)lxmn7)]qn)ox{ln)r|)\][XWP)r}q)x~*
 '''
+Secret_Message="Lxwp{j}~uj}rxw|*)bx~)l{jltnm)}qn)lxmn7)]qn)ox{ln)r|)\][XWP)r}q)x~*"
+for i in range(-20,21,1):
+    check=len(Secret_Message)
+    decrypt=""
+    for c in Secret_Message:
+        id = ord(c)
+        id+=i
+        if id==32 or id==33 or id==46 or 45<=id<=90 or 97<=id<=122:
+            check-=1
+        if check ==0:
+            break
+        ch=chr(id)
+        decrypt+=ch
+    print(i," : ", decrypt)
 
 
 
