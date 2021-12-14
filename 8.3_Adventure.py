a28515ed2c6111f34d1431b44185f26a6b91917d
bakerkai <bakerkai@urbandaleschools.com>
2021-12-14 11:39:03 -0600

    confusing lists

--- 8.3_Adventure.py
+++ 8.3_Adventure.py
@@ -21,28 +21,32 @@ total_time = 0
 room_list = []
 item_list = []
 key = 'This key smells faintly of AXE deoderant.'
-room = ["\033[1;31;48m" "You are in your bedroom. There is a box with an unknown object.", 2, None, None, None] # 3 IS MEANT FOR STAIRS
+room = ["\033[1;31;48m" "You are in your bedroom. There is a box with an unknown object. N E W S P for backpack Q to quit X to search", 1, 2, 3, 4]
 room_list.append(room)
-room = ["\033[1;31;48m" "Bedroom 2, this room is silent and very dark, weirdly dark.", 2, None, None, None]
+room = ["\033[1;31;48m" "Bedroom 2, this room is silent and very dark, weirdly dark. N E W S P for backpack Q to quit X to search", 1, 2, 3, 4]
 room_list.append(room)
-room = ["\033[1;31;48m" "Bedroom 3, this room sounds sounds of a train, also known as your dad's snoring.",2, None, None, None]
+room = ["\033[1;31;48m" "Bedroom 3, this room sounds sounds of a train, also known as your dad's snoring. N E W S P for backpack Q to quit X to search", 1, 2, 3, 4]
 room_list.append(room)
-room = ["\033[1;31;48m" "Bedroom 4, this room has a fan which is on and there is someone sleeping in the bedroom.",2, None, None, None]
+room = ["\033[1;31;48m" "Bedroom 4, this room has a fan which is on and there is someone sleeping in the bedroom. N E W S P for backpack Q to quit X to search", 1, 2, 3, 4]
 room_list.append(room)
-room = ["\033[1;31;48m" "Bathroom, this room is what it says it is. don't bother going there because there is nothing of interest for you there.",2, None, None, None]
+room = ["\033[1;31;48m" "Bathroom, this room is what it says it is. don't bother going there because there is nothing of interest for you there. N E W S P for backpack Q to quit X to search", 1, 2, 3, 4]
 room_list.append(room)
-room = ["\033[1;31;48m" "You have done it, this is the stairs to the ground floor. You are relieved and now have the ability to go and get the sacred ice juice.",2, None, None, None]
+room = ["\033[1;31;48m" "You have done it, this is the stairs to the ground floor. You are relieved and now have the ability to go and get the sacred ice juice. N E W S P for backpack Q to quit X to search", 1, 2, 3, 4]
 room_list.append(room)
-
+room = ["\033[1;31;48m" "You have done it, this is the stairs to the ground floor. You are relieved and now have the ability to go and get the sacred ice juice. N E W S P for backpack Q to quit X to search", 1, 2, 3, 4]
+room_list.append(room)
+# As soon as you leave your room, the door creaks closed and your attempt to get back into it is futile.
 current_room = 0
+
+#possibly add decrypt?
+#\033[1;33;48m" "N E W S P for backpack Q to quit X to search
 
 done = False
 while not done:
 
     print()
-    print("\033[1;32;48m",room_list[current_room][0])
-    bing = input("\033[1;33;48m" "Type \nN \nE \nW \nS \nP for backpack \nQ to quit \nX to search")
-
+    #print("\033[1;32;48m",room_list[current_room][0])
+    bing = input(room_list[current_room][0])
     #NORTH
     if bing.lower()=="n" or bing.lower() == "north":
         next_room = room_list[current_room][1]
@@ -82,13 +86,12 @@ while not done:
             print(item_list)
     #SEARCH
     elif bing.lower() == "x" or bing.lower() == "search":
-        print("You are searching in the area.")
-        if len(room_list) == 1:
+        print("You are searching in the area to see if there are any keys.")
+        if room_list ==  :
             item_list.append(key)
     #STUID
     else:
         print("\033[1;31;48m" "That didn't make any sense, can you actually read please? Or let me know that there is something wrong with my code")
-
 
 #  "\033[1;31;48m" Red
 #  "\033[1;32;48m" Green
