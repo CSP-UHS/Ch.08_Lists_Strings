36aea31eb9ff8901f34d9372a65670586bf5a86b
toricden <toricden@urbandaleschools.com>
2021-12-21 20:33:24 -0600

    Save ventiuno

--- 8.3_Adventure.py
+++ 8.3_Adventure.py
@@ -8,60 +8,84 @@ ADVENTURE PROGRAM
 '''
 room_list=[]
 #create room list
-room=["You are on the front porch",2,None,None,None]
+room=["You are in the Front Entrance",1,None,None,None]
 room_list.append(room)
-room=["You are in Bedroom 2",4,2,None,None]
+room=["You are in the Fishy Boi room",4,6,0,2]
 room_list.append(room)
-room=["You are in the South Hall",5,3,0,1]
+room=["You are in the Seal room",3,1,None,None]
 room_list.append(room)
-room=["You are in the dining room",6,None,None,2]
+room=["You are in Pingus bedroom",None,4,2,None]
 room_list.append(room)
-
+room=["You are in the Polar Bear room",7,5,1,3]
+room_list.append(room)
+room=["You are in the Kitchen",None,None,6,4]
+room_list.append(room)
+room=["You are in the Igloo",5,None,None,1]
+room_list.append(room)
+room=["You are on the Roof",None,None,4,None]
+room_list.append(room)
+room=[""]
 current_room=0
+fish=False
 done=False
 while not done:
     print()
     print(room_list[current_room][0])
-    bing=input("Type N,E,S,W for directions or Q to rage quit")
+    bing=input("Type N,E,S,W for directions, I to interact or Q to rage quit")
 
     #NORTH
-    if bing.lower==("n") or bing.lower==("north"):
+    if bing.lower()=="n" or bing.lower()=="north":
         nextroom=room_list[current_room][1]
-    if nextroom== None:
-        print("You cant go there dummy")
-    else:
-        current_room=nextroom
+        if nextroom== None:
+            print("You cant go there dummy")
+        else:
+            current_room=nextroom
 
         # EAST
-    elif bing.lower==("e") or bing.lower==("east"):
+    elif bing.lower()=="e" or bing.lower()=="east":
         nextroom = room_list[current_room][2]
-    if nextroom == None:
-        print("You cant go there dummy")
-    else:
-        current_room = nextroom
+        if nextroom == None:
+            print("You cant go there dummy")
+        else:
+            current_room = nextroom
 
         # SOUTH
-    elif bing.lower == ("s") or bing.lower == ("south"):
+    elif bing.lower() == "s" or bing.lower() == "south":
         nextroom = room_list[current_room][3]
-    if nextroom == None:
-        print("You cant go there dummy")
-    else:
-        current_room = nextroom
+        if nextroom == None:
+            print("You cant go there dummy")
+        else:
+            current_room = nextroom
 
         # WEST
-    elif bing.lower == ("w") or bing.lower == ("west"):
-    nextroom = room_list[current_room][4]
-    if nextroom == None:
-    print("You cant go there dummy")
-    else:
-        current_room = nextroom
+    elif bing.lower() == "w" or bing.lower() == "west":
+        nextroom = room_list[current_room][4]
+        if nextroom == None:
+            print("You cant go there dummy")
+        else:
+            current_room = nextroom
 
         #QUIT
 
-    elif bing.lower == ("q") or bing.lower == ("quit"):
-    done=True
+    elif bing.lower() == "q" or bing.lower() == "quit":
+        done=True
 
+        #INTERACT
+    elif bing.lower()=="i" and current_room==0 or bing.lower()=="interact" and current_room==0:
+        print()
+        print("Nothing to see here just a few penguin beaks")
+    elif bing.lower()=="i" and current_room==1 and fish==False or bing.lower()=="interact" and current_room==1 and fish==False:
+        print()
+        ans1=input(str("You look into the fish tanks and see one fish still on the bottom and is colored differently to the others, do you grab it? (y/n)"))
+        if ans1.lower()=="y" or ans1.lower()=="yes":
+                    print()
+                    print("The fish grows a pair of legs the second it gets out of water and walks into the seal room")
+                    print()
+                    fish=True
     #stupid
-    else: print("Invalid answer bud")
+    elif nextroom==current_room:
+        print("Invalid answer bud")
+    else:
+        print("Invalid answer bud")
 
 print("You're welcome for experiencing the best game")
