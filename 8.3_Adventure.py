03039a9b95cafd4644d9e72d1b5762cfe955e56a
toricden <toricden@urbandaleschools.com>
2022-01-06 05:07:24 -0600

    Save ventitres

--- 8.3_Adventure.py
+++ 8.3_Adventure.py
@@ -31,9 +31,17 @@ fish=False
 done=False
 firstentrytoseal=False
 sealkey=False
-health=20
+health=15
 inventory=""
 fishuse=False
+keyanswer=""
+pingubedanswer=""
+firstentrytopinger=False
+pingucodeanswer="ok"
+walrusrushanswer="c"
+okay="w"
+vile=False
+bleeding=False
 while not done:
     print()
     print(room_list[current_room][0])
@@ -83,7 +91,7 @@ while not done:
         #Status Check
     elif bing.lower()=="c" or bing.lower()=="condition":
         print()
-        print(health,"/ 20")
+        print(health,"/ 15")
         print()
         print(inventory)
         #INTERACT
@@ -131,21 +139,59 @@ while not done:
         print("The fish walks back and hops into it's tank")
         fishuse=True
     #stupid
-    elif current_room==3:
+    elif current_room==3 and firstentrytopinger==False:
         print("You enter what appears to be a bedroom for.... a penguin")
         print()
-    elif current_room==3 and sealkey==True and bing.lower()=="i" or current_room==3 and sealkey==True and bing.lower()=="interact":
+        firstentrytopinger=True
+    if current_room==3 and sealkey==True and bing.lower()=="i" or current_room==3 and sealkey==True and bing.lower()=="interact":
         keyanswer=(input("You see a box with a keyhole perfectly fitting the key you have, do you use it? (y/n)"))
-    if keyanswer.lower()=="y" or keyanswer.lower()=="yes":
-       sealkey==False
+    if keyanswer.lower()=="y" and vile==False or keyanswer.lower()=="yes" and vile==False :
        pingubedanswer=(input("you open the box only to find a 8 letter, 2 word phrase needed to unlock another box, do you try to attempt it? (y/n)"))
-    if pingubedanswer.lower()=="noot noot":
+    if pingubedanswer.lower()=="y" and vile==False or pingubedanswer.lower()=="yes" and vile==False:
+        pingucodeanswer=(input("Enter the code"))
+    if pingucodeanswer=="noot noot" and vile==False:
         print()
         print("The box swings open and reveals a vile of blue dense liquid")
         print()
         print("BLUE VILE ITEM ACQUIRED")
         inventory+="Blue Liquid"
+        vile=True
+    elif pingucodeanswer!="noot noot" and pingucodeanswer!="ok":
+        print()
+        print("WRONG! plays out of a speaker from the box and you get sent an electric shock taking half your hp!")
+        health/=2
+    if health==0 or health<0:
+        print()
+        print("You die from your wounds")
+        done=True
+    elif current_room==4 or nextroom==4:
+        print()
+        print("You enter the room only to be immediatdely attacked by a polar bear dealing 1 bleed damage per room for the rest of the game!")
+        bleeding=True
+    if bleeding==True and bing.lower=="n" or bleeding==True and bing.lower=="e" or bleeding==True and bing.lower=="s" or bleeding==True and bing.lower=="w":
+       health-=1
+    if current_room==4 and bing.lower()=="i":
+        print()
+        print("There doesn't seem to be anything to do in here currently other than being attacked by polar bears")
+    if current_room==5:
+       okay=input("Across the lab table you see a walrus staring at you menacingly, across the table you see a empty syringe do you try and grab it? (y/n)")
+    if okay=="y" and bleeding==True:
+        print("You reach over only for the walrus to recognize the smell of blood and pounce on you dealing 10 damage!")
+        health-=10
+    if okay=="y" and bleeding==False:
+        print()
+        walrusrushanswer=(input("You get closer to the syringe but the walrus seems more nervous, do you, A. Grab the syringe quick OR B. Continue to take it slow?"))
+    if walrusrushanswer.lower()=="a":
+        print()
+        print("You reach for the syringe but before you can even touch it the walrus pounces on you and starts mauling you causing your demise")
+        done=True
+    if walrusrushanswer.lower()=="b":
+        print()
+        print("The walrus relaxes as you grab the syringe and slowly back out to the corner")
+        print()
+        print("SYRINGE ITEM ACQUIRED")
+        inventory+=",Syringe"
 
 
 print()
-print("You're welcome for experiencing the best game")
+print("You're welcome for experiencing the best game ever")
