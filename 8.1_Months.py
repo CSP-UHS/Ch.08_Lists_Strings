84ad6530c27ad224daf9edadad98df4ba69c61b1
pattisonsam <pattisonsam@urbandaleschools.com>
2022-12-02 08:42:14 -0600

    8.1 done

--- 8.1_Months.py
+++ 8.1_Months.py
@@ -8,3 +8,15 @@ Once the user quits, print "Goodbye!"
 '''
 
 months = "JanFebMarAprMayJunJulAugSepOctNovDec"
+done = False
+while not done:
+    try:
+        m = int(input("Please enter a month. (1-12): "))
+    except ValueError:
+        print("Please enter an integer.")
+        continue
+    if 0<m<13:
+        print(months[m * 3 - 3:m * 3])
+    else:
+        break
+print("You Lose")
